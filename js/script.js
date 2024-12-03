b4a1ba74cb0df912f4e1e63fefd9fed45d0b84dc
Bella_Amine <amine19.bella@gamil.com>
2024-12-03 21:54:02 +0100

    finally

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
+var div=document.getElementById("image");
 function showImgInBox (CurrentElment){
-    var div=document.getElementById("image");
     var urlImg=CurrentElment.src;
     var urlAlt=CurrentElment.alt;
 
@@ -10,7 +10,6 @@ function showImgInBox (CurrentElment){
 
 
 function showMsgInBox (){
-    var div=document.getElementById("image");
     div.style.backgroundImage= "url('')";
     div.textContent="Hover over an image below to display here.";
 
@@ -24,4 +23,17 @@ var links=document.getElementsByTagName("a");
 for(let i=0;i<links.length;i++){
     links[i].setAttribute("tabindex",-1);
 }
+
+function loadingPage() {
+    const loadingDiv = document.getElementById("loading");
+    // Wait 3 seconds before hiding the loading indicator
+    setTimeout(() => {
+        if (loadingDiv) {
+            loadingDiv.classList.add("hidden"); // Add the hidden class
+            loadingDiv.style.display = "none"; // Ensure it doesn't block events
+        }
+    }, 1000); // 3000 milliseconds = 3 seconds
+}
+
+
 
