4d1c620166e8b491af004126487bb46544453054
Bella_Amine <amine19.bella@gamil.com>
2024-12-03 20:39:27 +0100

    comm1

---
+++ js/script.js
@@ -0,0 +1,28 @@
+function showImgInBox (CurrentElment){
+    var div=document.getElementById("image");
+    var urlImg=CurrentElment.src;
+    var urlAlt=CurrentElment.alt;
+
+    div.style.backgroundImage= "url('"+urlImg+"')";
+    div.textContent=urlAlt;
+
+}
+
+
+function showMsgInBox (){
+    var div=document.getElementById("image");
+    div.style.backgroundImage= "url('')";
+    div.textContent="Hover over an image below to display here.";
+
+}
+
+    var images=document.getElementsByClassName("preview");
+    for(let i=0;i<images.length;i++){
+        images[i].setAttribute("tabindex",0);
+    }
+
+    var links=document.getElementsByTagName("a");
+    for(let i=0;i<links.length;i++){
+        links[i].setAttribute("tabindex",-1);
+    }
+
